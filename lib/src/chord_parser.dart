feeb91b7110294b54dfe14828d01933ed5f9d57e
auhsor <auhsor@gmail.com>
2022-03-26 16:01:19 +1100

    Update chord_parser.dart
    My proposed changes to properly fix the overflow issues. I have combined both the original function and my function.
    
    The code has turned out much more complex than I was hoping as I kept running into little edge cases. Regardless, it seems to work better (ie no overflows errors) for me. I'm still not 100% sure that this is the best way to go forward with this but I need to commit it as it keeps playing on my mind! It can always be fixed later I guess.
    
    I'm no flutter expert so I'm not sure on the correct way of naming variables (with the underscore etc) or writing comments.
    
    I have also removed the need for the widgetPadding parameter by calculating the width directly using a LayoutBuilder in the lyrics_renderer file.

--- lib/src/chord_parser.dart
+++ lib/src/chord_parser.dart
@@ -13,115 +13,211 @@ class ChordProcessor {
     required String text,
     required TextStyle lyricsStyle,
     required chordStyle,
-    widgetPadding = 0,
+    required double widgetWidth,
     int transposeIncrement = 0,
   }) {
-    final lines = text.split('\n');
+    List<ChordLyricsLine> _chordLyricsLines = [];
+    ChordLyricsLine _chordLyricsLine = ChordLyricsLine([], '');
+    ChordLyricsLine _chordLyricsBuffer = ChordLyricsLine([], '');
+    String lyricsFromLastChord = '';
+    bool chordYetInLine = false;
+    bool chordYetInBuffer = false;
+    String lyricsFromNewLineToFirstChord = '';
+    String lyricsFromBufferToFirstChord = '';
+    String chordsSoFar = '';
+    String previousChord = '';
+    String character = '';
+    String transposedChord = '';
+    double leadingSpace = 0;
+    double sizeOfLeadingLyrics = 0;
+    bool chordHasStarted = false;
 
-    bool _chordHasStartedOuter = false;
-    String _currentCharacters = '';
-    int _lastSpace = 0;
-    String _currentLine = '';
-    String _character = '';
-    double _media = MediaQuery.of(context).size.width;
-    int _characterIndex = 0;
+    //Keep populating lyrics and chords until it gets too long for the screen.
+    //
+    //Keep a buffer that gets added to the actual line once we hit a space.
+    //If we have a chord or lyric that causes overflow, break and write current line.
+    //Keep the buffer (adjusting variables appropriately) and use for next line.
+    //Need to keep some variables to track the lyrics at the start of line or buffer.
+    //This helps when we need to adjust the leadingSpace before the first chord in the new line.
+    for (var i = 0; i < text.length; i++) {
+      character = text[i];
+      if (character == ']') {
+        //End of chord detected
 
-    //list to store our updated lines without overflows
-    final _newLines = <String>[];
+        //Transpose chord if required
+        transposedChord = transposeChord(
+          chordsSoFar,
+          transposeIncrement,
+        );
 
-    //loop through the lines
-    for (var i = 0; i < lines.length; i++) {
-      _characterIndex = 0;
-      _currentCharacters = '';
-      _currentLine = lines[i];
+        //Reset variables
+        chordsSoFar = '';
+        chordHasStarted = false;
 
-      //check if we have a long line
-      if (textWidth(_currentLine, lyricsStyle) >= _media) {
-        print('found a big line $_currentLine');
-        //work our way through the line and split when we need to
-        for (var j = 0; j < _currentLine.length; j++) {
-          _character = _currentLine[j];
-          if (_character == '[')
-            _chordHasStartedOuter = true;
-          else if (_character == ']')
-            _chordHasStartedOuter = false;
-          else if (!_chordHasStartedOuter) {
-            _currentCharacters += _character;
-            if (_character == ' ') {
-              //use this marker to only split where there are spaces. We can trim later.
-              _lastSpace = j;
+        //Check width of chord buffer
+        double chordBufferWidth = 0;
+        String textToCheck = '';
+        _chordLyricsBuffer.chords.forEach((element) {
+          chordBufferWidth += element.leadingSpace;
+          textToCheck += element.chordText;
+        });
+        chordBufferWidth += textWidth(textToCheck, chordStyle);
+
+        final thisChordWidth = textWidth(transposedChord, chordStyle);
+
+        if (textWidth(_chordLyricsLine.lyrics, lyricsStyle) +
+                chordBufferWidth +
+                thisChordWidth >
+            widgetWidth) {
+          //overflow detected with the chords
+
+          //Add current line
+          _chordLyricsLines.add(_chordLyricsLine);
+
+          //reset variables
+          _chordLyricsLine = ChordLyricsLine([], '');
+          chordHasStarted = false;
+          leadingSpace = 0;
+
+          previousChord = transposedChord;
+
+          //Add current chord to buffer
+          _chordLyricsBuffer.chords.add(Chord(leadingSpace, transposedChord));
+
+          //Adjust first chord if needed
+          if (chordYetInBuffer) {
+            _chordLyricsBuffer.chords.first.leadingSpace =
+                textWidth(lyricsFromBufferToFirstChord, lyricsStyle);
+            chordYetInLine = true;
+          }
+
+          //Add buffer to line
+          _chordLyricsLine.chords.addAll(_chordLyricsBuffer.chords);
+          _chordLyricsLine.lyrics += _chordLyricsBuffer.lyrics;
+
+          //Clear buffer
+          _chordLyricsBuffer.lyrics = '';
+          _chordLyricsBuffer.chords.clear();
+          chordYetInBuffer = false;
+        } else {
+          //Adding Chord to buffer
+
+          //Check to see if we are at the first chord in a new line
+          final lyricsToCheck = chordYetInLine
+              ? lyricsFromLastChord
+              : lyricsFromNewLineToFirstChord;
+          sizeOfLeadingLyrics = textWidth(lyricsToCheck, lyricsStyle);
+
+          //Find the leading space of the chord based on lyrics and previous chord
+          final lastChordWidth = textWidth(previousChord, chordStyle);
+          leadingSpace = (sizeOfLeadingLyrics - lastChordWidth);
+          leadingSpace = max(0, leadingSpace);
+
+          //Add the chord to the buffer
+          _chordLyricsBuffer.chords.add(Chord(leadingSpace, transposedChord));
+
+          previousChord = transposedChord;
+          chordYetInBuffer = true;
+          chordYetInLine = true;
+        }
+
+        lyricsFromLastChord = '';
+      } else if (character == '[') {
+        chordHasStarted = true;
+      } else {
+        if (chordHasStarted) {
+          chordsSoFar += character;
+        } else {
+          //This character is a lyric and not a chord
+          if (textWidth(
+                  _chordLyricsLine.lyrics +
+                      _chordLyricsBuffer.lyrics +
+                      character,
+                  lyricsStyle) >
+              widgetWidth) {
+            //Overflow detected with the lyrics.
+
+            //Add current line
+            _chordLyricsLines.add(_chordLyricsLine);
+
+            //reset variables
+            _chordLyricsLine = ChordLyricsLine([], '');
+            chordHasStarted = false;
+
+            lyricsFromLastChord += character;
+
+            //Adjust first chord if needed
+            if (chordYetInBuffer) {
+              _chordLyricsBuffer.chords.first.leadingSpace =
+                  textWidth(lyricsFromBufferToFirstChord, lyricsStyle);
             }
 
-            //This is the point where we need to split
-            //widgetPadding has been added as a parameter to be passed from the build function
-            //It is intended to allow for padding in the widget when comparing it to screen width
-            //An additional buffer of around 10 might be needed to definitely stop overflow (ie. padding + 10).
-            if (textWidth(_currentCharacters, lyricsStyle) + widgetPadding >=
-                _media) {
-              _newLines.add(
-                  _currentLine.substring(_characterIndex, _lastSpace).trim());
-              _currentCharacters = '';
-              _characterIndex = _lastSpace;
+            //Add buffer to line
+            _chordLyricsLine.chords.addAll(_chordLyricsBuffer.chords);
+            _chordLyricsLine.lyrics += _chordLyricsBuffer.lyrics + character;
+
+            //Flag if there is a chord in the line.
+            if (_chordLyricsLine.chords.isEmpty) {
+              chordYetInLine = false;
+              previousChord = '';
+            } else {
+              chordYetInLine = true;
+            }
+
+            //Clear buffer
+            _chordLyricsBuffer.lyrics = '';
+            _chordLyricsBuffer.chords.clear();
+            chordYetInBuffer = false;
+
+            //Now on a new line so this is needed
+            lyricsFromNewLineToFirstChord = _chordLyricsLine.lyrics;
+          } else if (character == '\n') {
+            //New line detected
+
+            //Add line to lines
+            _chordLyricsLines.add(_chordLyricsLine);
+
+            //Add buffer to next line
+            _chordLyricsLine.chords.addAll(_chordLyricsBuffer.chords);
+            _chordLyricsLine.lyrics += _chordLyricsBuffer.lyrics;
+
+            //Reset buffers. As this is a clean break we can reset all variables.
+            lyricsFromNewLineToFirstChord = '';
+            lyricsFromBufferToFirstChord = '';
+            lyricsFromLastChord = '';
+            _chordLyricsBuffer.lyrics = '';
+            _chordLyricsBuffer.chords.clear();
+            chordYetInLine = false;
+            chordYetInBuffer = false;
+            _chordLyricsLine = ChordLyricsLine([], '');
+            chordHasStarted = false;
+            previousChord = '';
+          } else {
+            //Add lyric to buffer
+            _chordLyricsBuffer.lyrics += character;
+            lyricsFromLastChord += character;
+            if (!chordYetInBuffer) lyricsFromBufferToFirstChord += character;
+            if (!chordYetInLine) lyricsFromNewLineToFirstChord += character;
+
+            if (character == ' ') {
+              //Found a space. Write current buffer to the line.
+              _chordLyricsLine.chords.addAll(_chordLyricsBuffer.chords);
+              _chordLyricsLine.lyrics += _chordLyricsBuffer.lyrics;
+
+              //Reset buffers and variables
+              lyricsFromBufferToFirstChord = '';
+              _chordLyricsBuffer.lyrics = '';
+              _chordLyricsBuffer.chords.clear();
+              chordYetInBuffer = false;
             }
           }
         }
-        //add the rest of the long line
-        _newLines.add(_currentLine
-            .substring(_characterIndex, _currentLine.length)
-            .trim());
-      } else {
-        //otherwise just add the regular line
-        _newLines.add(_currentLine.trim());
       }
     }
 
-    List<ChordLyricsLine> _chordLyricsLines =
-        _newLines.map<ChordLyricsLine>((line) {
-      ChordLyricsLine _chordLyricsLine = ChordLyricsLine([], '');
-      String _lyricsSoFar = '';
-      String _chordsSoFar = '';
-      bool _chordHasStarted = false;
-      line.split('').forEach((character) {
-        if (character == ']') {
-          final sizeOfLeadingLyrics = textWidth(_lyricsSoFar, lyricsStyle);
-
-          final lastChordText = _chordLyricsLine.chords.isNotEmpty
-              ? _chordLyricsLine.chords.last.chordText
-              : '';
-
-          final lastChordWidth = textWidth(lastChordText, chordStyle);
-          // final sizeOfThisChord = textWidth(_chordsSoFar, chordStyle);
-
-          double leadingSpace = 0;
-          leadingSpace = (sizeOfLeadingLyrics - lastChordWidth);
-
-          leadingSpace = max(0, leadingSpace);
-
-          final transposedChord = transposeChord(
-            _chordsSoFar,
-            transposeIncrement,
-          );
-
-          _chordLyricsLine.chords.add(Chord(leadingSpace, transposedChord));
-          _chordLyricsLine.lyrics += _lyricsSoFar;
-          _lyricsSoFar = '';
-          _chordsSoFar = '';
-          _chordHasStarted = false;
-        } else if (character == '[') {
-          _chordHasStarted = true;
-        } else {
-          if (_chordHasStarted) {
-            _chordsSoFar += character;
-          } else {
-            _lyricsSoFar += character;
-          }
-        }
-      });
-
-      _chordLyricsLine.lyrics += _lyricsSoFar;
-
-      return _chordLyricsLine;
-    }).toList();
+    //Now at the end of the text. Save the buffer as a line.
+    _chordLyricsLine.lyrics += _chordLyricsBuffer.lyrics;
+    _chordLyricsLines.add(_chordLyricsLine);
 
     return ChordLyricsDocument(_chordLyricsLines);
   }
@@ -139,6 +235,8 @@ class ChordProcessor {
 
   /// Transpose the chord text by the given increment
   String transposeChord(String chord, int increment) {
+    if (increment == 0) return chord;
+
     final cycle = [
       "C",
       "C#",
